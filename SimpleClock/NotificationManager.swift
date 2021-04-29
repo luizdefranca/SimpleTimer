7c52c9da9e3679015915196f38301f5c7a98016b
LuizRamos <luizramospe@hotmail.com>
2021-04-28 21:56:11 -0300

    deleted the breakpoint on TimerViewController

--- SimpleClock/NotificationManager.swift
+++ SimpleClock/NotificationManager.swift
@@ -10,68 +10,61 @@ import UserNotifications
 
 class NotificationManager: NSObject //, UNUserNotificationCenterDelegate
 {
-
+    
     static let shared = NotificationManager()
     var myNotificationCenter = UNUserNotificationCenter.current()
     private var identifiers = [String]()
     weak var timeViewDelegate : TimerViewControllerDelegate?
     private override init(){
         super.init()
-    //    myNotificationCenter.delegate = self
+        //    myNotificationCenter.delegate = self
     }
-
+    
     func askPermission() {
         myNotificationCenter.requestAuthorization(options: [.alert, .sound]) {granted, error in
-          if granted {
-            print("Notification allowed")
-          } else {
-            print("Notification Denied")
-          }
+            if granted {
+                print("Notification allowed")
+            } else {
+                print("Notification Denied")
+            }
         }
     }
     func scheduleNotification(id: Int, timeInterval: TimeInterval, title: String, body: String){
         identifiers.append("\(id)")
-            let content = UNMutableNotificationContent()
-            content.title = title
-            content.body = body
+        let content = UNMutableNotificationContent()
+        content.title = title
+        content.body = body
         content.sound = UNNotificationSound.default
-
-    //        let calendar = Calendar(identifier: .gregorian)
-    //        let components = calendar.dateComponents(
-    //            [.year, .month, .day, .hour, .minute],
-    //            from: dueDate)
-            let trigger = UNTimeIntervalNotificationTrigger(timeInterval: timeInterval, repeats: false)
-
-            let request = UNNotificationRequest(
-                identifier: "\(id)",
-                content: content,
-                trigger: trigger)
-
-            let mainQueue = OperationQueue.main
-
-            myNotificationCenter.add(request)
-
-
-        }
-
-
+        
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: timeInterval, repeats: false)
+        
+        let request = UNNotificationRequest(
+            identifier: "\(id)",
+            content: content,
+            trigger: trigger)
+        myNotificationCenter.add(request)
+        
+        
+    }
+    
+    
     func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
-
-
-            if let delegate = self.timeViewDelegate {
-                delegate.stopTimer()
-            }
+        
+        
+        if let delegate = self.timeViewDelegate {
+            delegate.stopTimer()
+        }
         print("notification finished")
     }
-
-
+    
+    
     func cancelNotification() {
         if identifiers.isEmpty {
             return
         }
         myNotificationCenter.removeDeliveredNotifications(withIdentifiers: identifiers)
         myNotificationCenter.removePendingNotificationRequests(withIdentifiers: identifiers)
-
+        
     }
     
 }
